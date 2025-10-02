ece4f0f52a149e3e7d60762b0477ed95d0daaa0d
Kajbundit <gadbandidmonta@gmail.com>
2025-10-02 15:08:13 +0700

    Update Bisection Methods

--- src/pages/methods/root/Bisection.jsx
+++ src/pages/methods/root/Bisection.jsx
@@ -1,8 +1,79 @@
+import '../../../styles/root/Bisection.css'
+import BisectionUtils from '../../../utils/root/Bisection'
 
+import { useState } from 'react'
 
 function Bisection() {
+
+    const [result, setResult] = useState([])
+
+    const cal = () => {
+
+        const fx = document.getElementById('fx').value
+        const xstart = parseFloat(document.getElementById('xstart').value)
+        const xend = parseFloat(document.getElementById('xend').value)
+        const Error = parseFloat(document.getElementById('error').value)
+
+        const result = BisectionUtils(xstart, xend, Error, fx)
+
+        if (Array.isArray(result)) {
+            setResult(result);
+        }
+        else {
+            alert("Cann't Solve please change.");
+        }
+
+    }
+
     return(
-        <h1>Bisection search</h1>
+        <div className='Bbody'>
+            <h1 className='Bh1'>Bisection search</h1>
+            <br/>
+
+            <label className='labelf'>f(x) : </label>
+            <input className='fbox' id='fx' type='text' defaultValue="x^4-13"></input>
+            <br/><br/><br/>
+
+            <label className='label'>X Start : </label>
+            <input className='xbox' id='xstart' type='number' defaultValue='1.5' step='1'></input>
+
+            <label className='label'> X End : </label>
+            <input className='xbox' id='xend' type='number' defaultValue='2' step='1'></input>
+
+            <label className='label'> Error : </label>
+            <input className='pbox' id='error' type='number' defaultValue='0.000001' step='0.000001'></input>
+            <br/><br/><br/>
+
+            <button className='calBotton' onClick={cal}>Calculate</button>
+            <br/><br/><br/>
+
+            <div className='Gtable'>
+                <table className='inGtable'>
+
+                    <thead>
+                        <tr>
+                            <th>iter</th>
+                            <th>x</th>
+                            <th>f(x)</th>
+                            <th>Error</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {result.map( (row, index) =>
+                            <tr key={index}>
+                                <td>{row.iter}</td>
+                                <td>{row.x}</td>
+                                <td>{row.y}</td>
+                                <td>{row.err}</td>
+                            </tr>
+                        )}
+                    </tbody>
+
+                </table>
+            </div>
+
+        </div>
     );
 }
 
---
+++ src/utils/root/Bisection.js
@@ -0,0 +1,81 @@
+import * as math from 'mathjs'
+
+function BisectionUtils (Xstart , Xend, Error, fx) {
+
+    const fxc = math.compile(fx)
+    const result = []
+
+    let i = 0;
+    let xl = Xstart
+    let xr = Xend
+    let fxl = fxc.evaluate( {x : xl} )
+    let fxr = fxc.evaluate( {x : xr} )
+
+    let xm = ( xl + xr ) / 2
+    let fxm = fxc.evaluate( {x : xm} )
+    let eps = Infinity
+
+    if (fxl * fxr < 0) {
+
+        result.push({
+            iter : i++,
+            x : format(xm),
+            y : format(fxm),
+            err : format(eps)
+        })
+
+        if (fxm * fxl < 0) {
+            xr = xm
+            fxr = fxm
+        }
+        else if (fxm * fxl > 0) {
+            xl = xm
+            fxl= fxm
+        }
+
+        while (eps > Error && xm !== 0) {
+            xm = ( xl + xr ) / 2
+            fxm = fxc.evaluate( {x : xm} )
+            
+            if (fxm * fxl < 0) {
+                eps = math.abs((xm - xr) / xm)
+                xr = xm
+                fxr = fxm
+            }
+            else if (fxm * fxl > 0) {
+                eps = math.abs((xm - xl) / xm)
+                xl = xm
+                fxl= fxm
+            }
+
+            result.push({
+                iter : i++,
+                x : format(xm),
+                y : format(fxm),
+                err : format(eps)
+            })
+
+        }
+
+    }
+
+    else {
+        return 404;
+    }
+
+    return result;
+
+}
+
+function format (num) {
+
+    if (Number.isInteger(num)) {
+        return num.toFixed(0)
+    }
+    else {
+        return num.toFixed(6)
+    }
+
+}
+
+export default BisectionUtils
